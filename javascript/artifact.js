1d29893cbae9d694c818c70d86c457de9f50d7ed
nicklaus_ <pedcremo@gmail.com>
2016-10-05 23:22:29 +0200

    Added context and stick without bouncing logic

--- javascript/artifact.js
+++ javascript/artifact.js
@@ -6,14 +6,15 @@
   *
  */
 
-var Artifact = function (id_artifact) {
+var Artifact = function (id_artifact,context) {
 
   this.imgObj = document.getElementById(id_artifact);
   this.state = "stop"; //startdbl,startclick
   this.speed = 3;
+  this.context = context;
   var self=this; //Artifici per fer funcionar setInterval
   this.getArtifactSelf = function(){return self;};
-
+  
   this.directions={
     NORTH:{dirX:0,dirY:-1},
     SOUTH:{dirX:0,dirY:1},
@@ -39,11 +40,11 @@ Artifact.prototype.move = function(){
 //Posicionem Bola de manera absoluta en X i Y i comprovem llímits
 Artifact.prototype.locate = function(x,y){
     //Ens eixim per dalt o per baix
-    if (y<=0 || y>=this.vpHeight-this.imgObj.height) {
+    if (y<=0 || y>=this.context.vpHeight-this.imgObj.height) {
         this.dirY=this.dirY*(-1);
     }
     //Ens eixim per dreta o esquerre
-    if (x<=0 || x>=this.vpWidth-this.imgObj.width) this.dirX=this.dirX*(-1);
+    if (x<=0 || x>=this.context.vpWidth-this.imgObj.width) this.dirX=this.dirX*(-1);
 
     this.imgObj.style.left = (Math.round(x))+ 'px';
     this.imgObj.style.top = (Math.round(y)) + 'px';
@@ -53,11 +54,14 @@ Artifact.prototype.locate = function(x,y){
  //Sortejem direcció i comencem a moure la pola
  Artifact.prototype.start = function(){
     var self=this.getArtifactSelf();
+    self.state = "run";
+    self.setDirection("NORTH_WEST");
     animate=setInterval(function(){self.move();}, 5);
  };
 
  //Parem la bola
  Artifact.prototype.stop = function(){
+     this.state = "stop";
      clearTimeout(animate);
  };
 
--- javascript/main.js
+++ javascript/main.js
@@ -3,56 +3,24 @@
  *
  *
  */
-//(function () {
-    var artifact = require('./artifact');
-    var utils = require('./utils');
-    var stick = require('./stick');
 
-    var vpWidth=window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-    var vpHeight=window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+var utils = require('./utils');
+var context = require('./context');
 
+//Once page has been completely loaded. Including images. We start the game
+window.onload=function(){
 
-           window.onload=function(){
-                var artifact_ = new artifact("bola");
-                var stick_ = new stick("stick","right");
-                artifact_.vpWidth=vpWidth;
-                artifact_.vpHeight=vpHeight;
-                artifact_.locate((vpWidth/2)-artifact_.imgObj.width,(vpHeight/2)-artifact_.imgObj.height);  //Posicionem pilota al mig
+    var context_ = new context();
 
-                var controlFunction=function(event){
-                    event.preventDefault();
-                    utils.clearSelection();
-                    if (artifact_.state.match("run")) {
-                        artifact_.state="stop";
-                        artifact_.stop();
-                    }else{
-                        switch (event.type){
-                            case "dblclick":
-                                if (artifact_.state !== "run"){
-                                    artifact_.state="run";
-                                    artifact_.setDirection("NORTH_WEST");
-                                }
-                                break;
-                            case "click":
-                                if (artifact_.state !== "runvertical"){
-                                    artifact_.state="runvertical";
-                                    artifact_.setDirection("NORTH");
-                                }
-                                break;
-                            default:
-                                if (artifact_.state !== "runhorizontal"){
-                                    artifact_.state="runhorizontal";
-                                    artifact_.setDirection("WEST");
-                                }
-                                break;
-                        }
-                        artifact_.start();
-                    }
-                };
+    var startGame=function(event){
+        event.preventDefault();
+        utils.clearSelection();
+        if (context_.state.match("run")){
+          context_.stop();
+        }else{
+          context_.start();
+        }
+    };
 
-                window.addEventListener("dblclick",controlFunction,false);
-                window.addEventListener("click",controlFunction,false);
-                window.addEventListener("keypress",controlFunction,false);
-
-            };
-//})();
+    window.addEventListener("keypress",startGame,false);
+};
