94671b763f6da763aa18a9ee34627a4ec10629c3
nicklaus_ <pedcremo@gmail.com>
2016-11-20 21:40:06 +0100

    Some heroku fix

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,6 @@ var server = express();
 
 server.use(express.static('frontend'));
 
-server.listen(3000, function(){
+server.listen(process.env.PORT || 3000, function(){
    console.log('Example app listening on port 3000!');
 });
